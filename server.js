11bbb05df5ac947dc736c46a5d65d2b9e4ab10a0
Jonathon Schwamman <schwamman@msn.com>
2019-08-27 13:59:24 -0500

    Added partial html files and styled index/show pages

--- server.js
+++ server.js
@@ -30,11 +30,20 @@ app.get('*', (request, response) => response.status(404).send('This route does n
 
 app.listen(PORT, () => console.log(`I know that you came to party baby, baby, baby, baby on port: ${PORT}`));
 
+//Function to handle errors
+function handleError(error, response) {
+  response.status(error.status || 500).send(error.message);
+}
+
+let httpRegex = /^(http:\/\/)/g;
+
 // HELPER FUNCTIONS
 function Book(bookData) {
   this.name = bookData.hasOwnProperty('title') ? bookData.title : 'Unknown Title';
   this.author = bookData.hasOwnProperty('authors') ? bookData.authors : 'Unknown Authors';
   this.description = bookData.hasOwnProperty('description') ? bookData.description : 'No Description';
+  this.isbn = bookData.hasOwnProperty('industryIdentifiers') ? bookData.industryIdentifiers[0].identifier : 'No ISBN';
+  this.image_url = bookData.hasOwnProperty('imageLinks') ? bookData.imageLinks.thumbnail.replace(httpRegex, 'https://') : 'https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg';
 }
 
 // No API key required
@@ -47,9 +56,17 @@ function createSearch(request, response) {
   if (request.body.search[1] === 'title') { url += `+intitle:${request.body.search[0]}`; }
   if (request.body.search[1] === 'author') { url += `+inauthor:${request.body.search[0]}`; }
 
+  
+
   superagent.get(url)
-    .then(apiResponse => apiResponse.body.items.map(bookResult => new Book(bookResult.volumeInfo)))
-    .then(results => response.render('pages/searches/show', {searchResults: results}));
+    .then(apiResponse => apiResponse.body.items.map(bookResult => {
+      return new Book(bookResult.volumeInfo);
+    }))
+    .then(results => {
+      const sliceIndex = results.length > 10 ? 10 : results.length;
+      return response.render('pages/searches/show', {searchResults: results.slice(0, sliceIndex)});
+    })
+    .catch(error => handleError(error, response));
     
   // how will we handle errors?
 }
